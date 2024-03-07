fbb187f0401e3cdcf3a89c41d6167ba1056933e2
Pratik Lamichhane <lamichhanepratik9@gmail.com>
2024-03-07 18:49:43 +0545

    Refactor search functionality and update styling

--- script.js
+++ script.js
@@ -2,33 +2,64 @@ let result = document.getElementById('result');
 let searchBtn = document.getElementById('search-btn');
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-let userInput = document.getElementById('user-input').value;
-
-fetch(url + "chicken")
-    .then((response) => response.json())
-    .then((data) => {
-    console.log(data);
-    let myMeal = data.meals[0];
-    console.log(myMeal);
-    console.log(myMeal.strMealThumb);
-    console.log(myMeal.strMeal);
-    console.log(myMeal.strArea);
-    console.log(myMeal.strInstructions);
-    let count = 1;
-    let myIngredients = [];
-    for ( let i in myMeal) {
-        let ingredient = "";
-        let measure = "";
-        if(i.startsWith("strIngredient") && myMeal[i] != "") {
-            ingredient = myMeal[i];
-            measure = myMeal["strMeasure" + count];
-            myIngredients.push(measure + " " + ingredient);
-            count++;
+searchBtn.addEventListener('click', () => {
+    let userInput = document.getElementById('user-input').value;
+    if (userInput.length === 0) {
+        result.innerHTML = `<h2 id="warning">Please enter a meal name</h2>`;
+    }
+    else{
+        fetch(url + userInput)
+        .then((response) => response.json())
+        .then((data) => {
+        console.log(data);
+        let myMeal = data.meals[0];
+        console.log(myMeal);
+        console.log(myMeal.strMealThumb);
+        console.log(myMeal.strMeal);
+        console.log(myMeal.strArea);
+        console.log(myMeal.strInstructions);
+        let count = 1;
+        let myIngredients = [];
+        for ( let i in myMeal) {
+            let ingredient = "";
+            let measure = "";
+            if(i.startsWith("strIngredient") && myMeal[i] != "") {
+                ingredient = myMeal[i];
+                measure = myMeal["strMeasure" + count];
+                myIngredients.push(measure + " " + ingredient);
+                count++;
+                }
             }
-        }
-    console.log(myIngredients);
+        console.log(myIngredients);
+        
+        result.innerHTML = `<img src="${myMeal.strMealThumb}">`;
+        result.innerHTML += `<h2>${myMeal.strMeal}</h2>`;
+        result.innerHTML += `<p>${myMeal.strArea}</p>`;
+        result.innerHTML += `<div id="ingredients-container"></div>`;
+        result.innerHTML += `
+        <div id="recipe-container">
+        <div class="close-recipe"><button id="close-recipe">X</button></div>
+        <pre id="instructions">${myMeal.strInstructions}</pre>
+        </div>
+        <button id="show-recipe">View Recipe</button>`
+        let ingredientsContainer = document.getElementById('ingredients-container');
+        let parent = document.createElement('ul');
+        let recipe = document.getElementById('recipe-container');
+        let closeRecipe = document.getElementById('close-recipe');
+        let showRecipe = document.getElementById('show-recipe');
     
-    result.innerHTML = `<img src="${myMeal.strMealThumb}">`;
-
-    })
-
+        myIngredients.forEach((i) => {
+            let child = document.createElement('li');
+            child.textContent = i;
+            parent.appendChild(child);
+            ingredientsContainer.appendChild(parent);
+        });
+    
+        closeRecipe.addEventListener('click', () =>
+        recipe.style.display = "none");
+        showRecipe.addEventListener('click', () =>
+        recipe.style.display = "block");
+        }); 
+    
+    }
+});
